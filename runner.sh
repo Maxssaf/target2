88c0d7e9253a31838b4b720202ff45af8112eef0
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-03-19 14:19:22 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -16,12 +16,12 @@ num_of_copies="${1:-1}"
 ## Restart script every N seconds (900s = 15m, 1800s = 30m, 3600s = 60m).
 ## It allows to download updates for mhddos_proxy, MHDDoS and target list.
 ## By default 900s (15m), can be passed as second parameter
-restart_interval="600"
+restart_interval="300"
 
 #parameters that passed to python scrypt
 threads="${2:-500}"
 threads="-t $threads"
-rpc="${3:-100}"
+rpc="${3:-1000}"
 rpc="--rpc $rpc"
 proxy_interval="1200"
 proxy_interval="-p $proxy_interval"
