2c17789b1d03b7a565ee798c5e996d864ba36bfa
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-03-26 21:38:29 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -60,7 +60,7 @@ do
             #echo $cmd_line
             echo $cmd_line $proxy_interval $threads $rpc
             cd ~/mhddos_proxy
-            python3 runner.py --debug $cmd_line $proxy_interval $threads $rpc&
+            python3 runner.py $cmd_line $proxy_interval $threads $rpc&
             echo -e "Attack started. Wait a few minutes for output"
    done
 echo -e "\nDDoS is up and Running, next update of targets list in $restart_interval\nSleeping\n"
