4c518df8e2bb58de93727aa21b4ba801927f3c11
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-04-02 16:34:16 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -25,7 +25,7 @@ rpc="${3:-1000}"
 rpc="--rpc $rpc"
 proxy_interval="1200"
 proxy_interval="-p $proxy_interval"
-stats_interval="60"
+stats_interval="120"
 
 #Just in case kill previous copy of mhddos_proxy
 pkill -f runner.py
