716d819617cd5e4da2fd8a297780b856c931a44b
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-04-03 01:57:31 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -67,10 +67,10 @@ do
    
  
  #echo -e "\nDDoS is up and Running, next update of targets list in $restart_interval\nSleeping\n"
- ifstat -i eth0 -t -b -S -n $stats_interval/$stats_interval | awk '$1 ~ /^[0-9]{2}:/{$2/=1024;$3/=1024;printf "[%s] %6.2f ↓MBit/s↓  %6.2f ↑MBit/s↑",$1,$2,$3}'&
+ ifstat -i eth0 -t -b -n  | awk '$1 ~ /^[0-9]{2}:/{$2/=1024;$3/=1024;printf "[%s] %6.2f ↓MBit/s↓  %6.2f ↑MBit/s↑\n",$1,$2,$3}'&
  sleep $restart_interval
  clear
- #echo -e "\nRESTARTING\nKilling old processes..."
+ #echo -e "\nRESTARTING\nKilling old processes..."  $stats_interval/$stats_interval
  pkill -f runner.py
  pkill -f ./start.py
  echo -e "Old processes have been killed - starting new ones"
