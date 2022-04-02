411f3c7e19bc28f19000a9d99587d996fcde7f07
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-04-02 23:26:52 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,22 +5,28 @@
 pkill -f runner.py
 pkill -f ./start.py
 
-sudo apt update -y
-sudo apt upgrade -y
+# Install git, python3, pip, mhddos_proxy, MHDDoS and updated proxy list.
+sudo apt update  -y
+sudo apt install python3 python3-pip  -y
+# for some virtual cloud systems based on debian (like GC)
+sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc  -y 
+sudo pip install --upgrade pip 
+
+
 # Install git, python3
-sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
-sudo apt install git -y
-sudo apt upgrade git -y
+#sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
+#sudo apt install git -y
+#sudo apt upgrade git -y
 sudo apt install wget -y
-sudo apt upgrade wget -y
-sudo apt install python3 -y
-sudo apt upgrade python3 -y
-sudo apt install python3-pip -y
-sudo apt upgrade python3-pip -y
+#sudo apt upgrade wget -y
+#sudo apt install python3 -y
+#sudo apt upgrade python3 -y
+#sudo apt install python3-pip -y
+#sudo apt upgrade python3-pip -y
 sudo apt install screen -y
-sudo apt upgrade screen -y
+#sudo apt upgrade screen -y
 sudo apt install curl -y
-sudo apt upgrade curl -y
+#sudo apt upgrade curl -y
 
 
 
