a4eec9fc2bc9e77b65aafe8bc6a7735ee7089803
Maxssaf <101875980+Maxssaf@users.noreply.github.com>
2022-04-02 19:11:44 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -67,7 +67,7 @@ do
    
  
  #echo -e "\nDDoS is up and Running, next update of targets list in $restart_interval\nSleeping\n"
- ifstat -i eth0 -t -b -n $stats_interval/$stats_interval | awk '$1 ~ /^[0-9]{2}:/{$2/=1024;$3/=1024;printf "[%s] %6.2f ↓MBit/s↓  %6.2f ↑MBit/s↑\n",$1,$2,$3}'&
+ ifstat -i eth0 -t -b -n $stats_interval/$stats_interval | awk '$1 ~ /^[0-9]{2}:/{$2/=1024;$3/=1024;printf "[%s] %8.2f ↓MBit/s↓  %8.2f ↑MBit/s↑\n",$1,$2,$3}'&
  sleep $restart_interval
  clear
  #echo -e "\nRESTARTING\nKilling old processes..."
